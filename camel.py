0146aaa5a47653adfbb94e41827e67ac62c93284
Ndumiso Zulu <ndumisimizulu@gmail.com>
2025-09-01 12:12:08 +0200

    Update camel.py

--- camel.py
+++ camel.py
@@ -1,10 +1,18 @@
 #camel
 
-camel = input("Input: ")
-snake = " "
-for i in range(len(camel)):
-    if camel[i].isupper():
-        snake = camel[:i] + "_" + camel[i:]
-        snake = snake.lower()
-    
-print(snake)
+def main():
+    print(camel_case())
+
+def camel_case():
+    camel = input("Input: ")
+    snake = ""
+    for char in camel:
+        if char.isupper():
+            snake += "_" + char.lower()
+        else:
+            snake += char
+
+    return snake
+
+if __name__ == "__main__":
+    main()
