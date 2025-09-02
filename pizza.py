6456b6d1c11fdf99bc424af7b495c4848fb7b0e3
Ndumiso Zulu <ndumisimizulu@gmail.com>
2025-09-02 12:28:47 +0200

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -1,21 +1,28 @@
 import sys
+import csv
+from tabulate import tabulate
 
-def lines_of_code():
-    if (len(sys.argv) > 3):
-        sys.exit("Too many arguments.")
+def main():
+    if (len(sys.argv) > 2):
+        sys.exit("Too many arguments")
     elif (len(sys.argv) < 2):
         sys.exit("Too few arguments")
     else:
-        sys.argv[1]
-        if (sys.argv[1].endswith(".py")):
-            with open(sys.argv[1], "r") as file:
-                count = 0
-                for line in file:
-                    if not (line.lstrip().startswith("#") or line.strip() == ""):
-                        count = count + 1
-        else:
-            sys.exit("Not a python file")
-    
-    print(count)
+        if (sys.argv[1].endswith(".csv")):
+            headings = []
+            content = []
+            
+            with open(sys.argv[1]) as file:
+                rows = csv.DictReader(file)
+                headings = rows.fieldnames
+                for row in rows:
+                    value = list(row.values())
+                    content.append(value)
 
-lines_of_code()
+            print(tabulate(content, headings, tablefmt="grid"))
+
+        else:
+            sys.exit("Wrong file format.")
+            
+if __name__ == "__main__":
+    main()
